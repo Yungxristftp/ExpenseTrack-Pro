d353bef1f73d65a1e6f19adc32fcfa9a76965cf2
Yungxristftp <126116534+Yungxristftp@users.noreply.github.com>
2024-03-11 10:51:51 -0500

    First Commit

--- src/main/java/com/mycompany/expensetrack/pro/ExpenseTrackPro.java
+++ src/main/java/com/mycompany/expensetrack/pro/ExpenseTrackPro.java
@@ -11,6 +11,6 @@ package com.mycompany.expensetrack.pro;
 public class ExpenseTrackPro {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("HI World!");
     }
 }
