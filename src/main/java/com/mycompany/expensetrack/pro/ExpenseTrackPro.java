5e26b3766dbf296270bb79e37a99e5472cd7e932
Yungxristftp <126116534+Yungxristftp@users.noreply.github.com>
2024-03-11 10:23:09 -0500

    First Commit

---
+++ src/main/java/com/mycompany/expensetrack/pro/ExpenseTrackPro.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.expensetrack.pro;
+
+/**
+ *
+ * @author User
+ */
+public class ExpenseTrackPro {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
